0410bb344efdae50dc57d92ab7bc72f7e9bd72e9
Colin E <colin.eberhardt@gmail.com>
2021-01-26 14:06:18 +0000

    changed the module entry-point

--- index.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-const loader = require("@assemblyscript/loader");
-const imports = { env: { log: (value) => console.log(value) } };
-const wasmModule = loader.instantiateSync(
-  fs.readFileSync(__dirname + "/build/untouched.wasm"),
-  imports
-);
-module.exports = wasmModule.exports;
